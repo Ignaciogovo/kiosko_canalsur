6187cd479fa763f9f03eaa7aa07bca16569afe0c
ignaciogovo <ignaciogovo@gmail.com>
2025-07-08 16:33:54 +0200

    Generar archivos básicos

---
+++ kiosk.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+export DISPLAY=:0
+sleep 5
+xset s noblank
+xset s off
+xset -dpms
+unclutter -idle 0.5 -root &
+sed -i 's/"exited_cleanly":false/"exited_cleanly":true/' ~/.config/chromium/Default/Preferences
+sed -i 's/"exit_type":"Crashed"/"exit_type":"Normal"/' ~/.config/chromium/Default/Preferences
+/usr/bin/chromium-browser --noerrdialogs --disable-infobars --kiosk https://www.youtube.com/canalsur/live
